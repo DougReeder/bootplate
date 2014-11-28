52eb2bde56635ad66cc9bdf5bd72cebbf375c10d
P. Douglas Reeder <reeder.29@gmail.com>
2014-11-28 00:11:34 -0500

    Using enyo-webos, attempt to subscribe to luna://com.palm.wifi/findnetworks

--- source/App.js
+++ source/App.js
@@ -3,15 +3,38 @@ enyo.kind({
 	kind: "FittableRows",
 	fit: true,
 	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
+		{kind: "onyx.Toolbar", content: "luna://com.palm.wifi/findnetworks"},
 		{kind: "enyo.Scroller", fit: true, components: [
 			{name: "main", classes: "nice-padding", allowHtml: true}
 		]},
 		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
+   			{kind: "onyx.Button", content: "Subscribe", ontap: "subscribeTap"}
 		]}
 	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
+	subscribeTap: function(inSender, inEvent) {
+		var enyoWebosVersion = enyo && enyo.version && enyo.version["enyo-webos"];
+		this.log("enyo-webos version:", enyoWebosVersion);
+		
+        var wifiRequest = new enyo.ServiceRequest({service: "luna://com.palm.wifi", method: "findnetworks", subscribe: true, resubscribe: false});
+		this.$.main.addContent("enyo-webos version: " + enyoWebosVersion + '     timeout: ' + wifiRequest.timeout + '<br>');
+        var wifiParameters = {};
+        console.log("findnetworks", wifiParameters);
+        wifiRequest.go(wifiParameters);
+        wifiRequest.response(handleWifiResponse.bind(this));
+        wifiRequest.error(handleWifiFailure.bind(this));
+        
+        function handleWifiResponse(inSender, inResponse) {
+        	console.log("handleWifiResponse:", inResponse);
+        	if (inResponse.foundNetworks) {
+        		this.$.main.addContent(inResponse.foundNetworks.length + ' networks found<br>');
+        	} else {
+        		this.$.main.addContent(JSON.stringify(inResponse) + '<br>');
+        	}
+        }
+        
+        function handleWifiFailure(inSender, inResponse) {
+        	console.error("handleWifiFailure", inResponse);
+        	this.$.main.addContent(JSON.stringify(inResponse) + '<br>');
+        }
 	}
 });
